102b05726728fc069baf9e48362ced151076c0b4
Voidfractur <73500723+Voidfractur@users.noreply.github.com>
2021-01-17 11:01:58 -0600

    Base Datos

--- cine (1).sql
+++ cine (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-01-2021 a las 19:43:52
+-- Tiempo de generación: 17-01-2021 a las 17:51:18
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.0
 
@@ -25,9 +25,9 @@ DELIMITER $$
 --
 -- Procedimientos
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `RegistrarCleinte` (`nom_per` VARCHAR(45), `apepa_per` VARCHAR(45), `apema_per` VARCHAR(45), `correo_per` VARCHAR(45), `dierccion_per` TEXT(60), `fechaNac_per` DATE, `phone_per` VARCHAR(11), `correo` VARCHAR(50), `pass` VARCHAR(50))  begin
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RegistrarCliente` (`nom_per` VARCHAR(45), `apepa_per` VARCHAR(45), `apema_per` VARCHAR(45), `dierccion_per` TEXT(60), `fechaNac_per` DATE, `phone_per` VARCHAR(11), `correo` VARCHAR(50), `pass` VARCHAR(50))  begin
  INSERT INTO `persona` (`nom_per`, `apepa_per`, `apema_per`, `correo_per`, `dierccion_per`, `fechaNac_per`, `phone_per`)
- VALUES (nom_per, apepa_per, apema_per, correo_per, dierccion_per, fechaNac_per, phone_per);
+ VALUES (nom_per, apepa_per, apema_per, correo, dierccion_per, fechaNac_per, phone_per);
 set @per =(select max(id_per)from persona where id_per= id_per and apepa_per=apepa_per and apema_per=apema_per and correo_per=correo_per and dierccion_per=dierccion_per and fechaNac_per=fechaNac_per and phone_per=phone_per);
  INSERT INTO `usuario` (`correo`, `pass`, `id_per`, `tipo_usu`) VALUES (correo, pass, @per, 'CLIENTE');
  end$$
@@ -50,8 +50,10 @@ CREATE TABLE `peliculas` (
   `titulo` varchar(100) NOT NULL,
   `director` varchar(100) NOT NULL,
   `sinopsis` text NOT NULL,
+  `duracion` int(11) NOT NULL,
   `fechaEstreno` date NOT NULL,
   `fechaCartele` date NOT NULL,
+  `Fechafincartelera` date NOT NULL,
   `URLposter` text NOT NULL,
   `URLtrailer` text NOT NULL,
   `URLweboficial` text DEFAULT NULL,
@@ -59,6 +61,17 @@ CREATE TABLE `peliculas` (
   `paisorigen` varchar(100) NOT NULL,
   `estudioproductor` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `peliculas`
+--
+
+INSERT INTO `peliculas` (`id_pelis`, `titulo`, `director`, `sinopsis`, `duracion`, `fechaEstreno`, `fechaCartele`, `Fechafincartelera`, `URLposter`, `URLtrailer`, `URLweboficial`, `idiomas`, `paisorigen`, `estudioproductor`) VALUES
+(1, 'ASDADS', 'ASDASD', 'ASD', 11, '0000-00-00', '2021-01-13', '2021-01-04', 'ASDASD', 'ASDADS', 'ASDADS', 'ADS', 'ASD', 'AD'),
+(2, 'ASDASD', 'ASDADS', 'ASDASD', 11, '0000-00-00', '2021-01-28', '2021-01-03', 'ASDASD', 'ADSADS', 'ADSADS', 'ADSDAS', 'ADSDSA', 'ADSDAS'),
+(3, 'ASDASD', 'ASDADS', 'ASDASD', 11, '0000-00-00', '2021-01-28', '2021-01-03', 'ASDASD', 'ADSADS', 'ADSADS', 'ADSDAS', 'ADSDSA', 'ADSDAS'),
+(4, 'ASDASD', 'ASDADS', 'ASDASD', 11, '2021-01-07', '2021-01-28', '2021-01-03', 'ASDASD', 'ADSADS', 'ADSADS', 'ADSDAS', 'ADSDSA', 'ADSDAS'),
+(5, 'qwe', 'ASDqweADS', 'qwe', 11, '2021-02-04', '2021-02-04', '2021-02-04', 'qwe', 'ADSAqweDS', 'qwe', 'qwe', 'qwe', 'qwe');
 
 -- --------------------------------------------------------
 
@@ -82,7 +95,9 @@ CREATE TABLE `persona` (
 --
 
 INSERT INTO `persona` (`id_per`, `nom_per`, `apepa_per`, `apema_per`, `correo_per`, `dierccion_per`, `fechaNac_per`, `phone_per`) VALUES
-(1, 'nom_per varchar(45)', 'apepa_per varchar(45)', 'apema_per varchar(45)', 'correo_per varchar(45)', 'dierccion_per text(60)', '2021-01-15', 'phone_per v');
+(1, 'nom_per varchar(45)', 'apepa_per varchar(45)', 'apema_per varchar(45)', 'correo_per varchar(45)', 'dierccion_per text(60)', '2021-01-15', 'phone_per v'),
+(2, 'Leonardo', 'Camacho', 'Garcia', 'leonardo.blacker@gmail.com', 'av miguel hidalgo', '0000-00-00', '7151185506'),
+(3, 'Emmanuel', 'Camacho', 'Garcia', 'emmauel@hotmail.com', 'Av hidalgo', '0000-00-00', '7151185506');
 
 -- --------------------------------------------------------
 
@@ -117,7 +132,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`usu`, `correo`, `pass`, `id_per`, `tipo_usu`) VALUES
-(1, 'correo varchar(50)', 'pass varchar(50)', 1, 'CLIENTE');
+(1, 'correo varchar(50)', 'pass varchar(50)', 1, 'CLIENTE'),
+(2, 'leonardo.blacker@gmail.com', '$2y$10$8ppEVWC.I7exHUvSJoIZCuCZSwmSLkUuBaoewX0iJR5', 2, 'CLIENTE'),
+(3, 'emmauel@hotmail.com', '$2y$10$viehUYqYjSVuC69Uuj1SIuIbD7AsL7zQe7RWuR7cXKH', 3, 'CLIENTE');
 
 -- --------------------------------------------------------
 
@@ -131,7 +148,9 @@ CREATE TABLE `ventaboleto` (
   `asiento_vb` int(11) NOT NULL,
   `fechaventa_vb` date NOT NULL,
   `id_per` int(11) NOT NULL,
-  `id_pelis` int(11) NOT NULL
+  `id_pelis` int(11) NOT NULL,
+  `FechaVer` date NOT NULL,
+  `horaVer` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -176,13 +195,13 @@ ALTER TABLE `ventaboleto`
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  MODIFY `id_pelis` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pelis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `id_per` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_per` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `tienda`
@@ -194,7 +213,7 @@ ALTER TABLE `tienda`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `usu` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `usu` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `ventaboleto`
